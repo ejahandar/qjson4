bb546a58185c3884cee2541a3c751bfe6e57dd96
Ebrahim <ee@sharif.edu>
2018-03-14 23:58:14 +0330

    fixing sequence number type / fixing QWebsocket/QJson for QT4

--- QJsonDocument.h
+++ QJsonDocument.h
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef QJSON_DOCUMENT_H_
 #define QJSON_DOCUMENT_H_
 
-#include <QtCore/QtGlobal>
+#include <QtGlobal>
 
 #if QT_VERSION >= 0x050000
 #include <QtCore/QJsonDocument>
